bc2115ca97bb759f29cc06074f57226f05d476ab
Dickson Mwendia <64727760+Dickson-Mwendia@users.noreply.github.com>
2025-06-27 19:12:34 +0300

    Add Vercel analhytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,12 @@
 import '../styles/globals.css'
 import Footer from '../components/Footer'
+import { Analytics } from '@vercel/analytics/react'
 
 export default function App({ Component, pageProps }) {
   return (
     <div className="flex flex-col min-h-screen bg-navy">
       <Component {...pageProps} />
+      <Analytics />
       <footer className="mt-auto">
         <Footer />
       </footer>
